6b9a0b4daae904c3f498f6232e6cb46cc4e04807
TabsOverSpaces4 <guptaharsh.2401@gmail.com>
2021-07-16 07:17:38 +0530

    Firebase Initiated

--- lib/main.dart
+++ lib/main.dart
@@ -3,19 +3,24 @@ import 'package:planzee/pages/LSOptions.dart';
 import 'package:planzee/pages/Signin.dart';
 import 'package:planzee/pages/Signup.dart';
 import 'package:planzee/pages/about.dart';
+import 'package:planzee/pages/home.dart';
+import 'package:planzee/pages/info.dart';
+import 'package:planzee/pages/landingpage.dart';
 import 'package:planzee/pages/loading.dart';
+import 'package:planzee/pages/profile.dart';
 
 
 void main() => runApp(MaterialApp(
-      initialRoute: '/login',
+      initialRoute: '/landingpage',
       routes: {
         '/': (context) => Loading(),
+        '/landingpage': (context) => Landingpage(),
         '/login': (context) => LSOptions(),
         '/signin': (context) => SignIn(),
         '/signup': (context) => SignUp(),
-        // '/profile': (context) => Profile(),
-        // '/info': (context) => Info(),
-        // '/home': (context) => Homepage(),
+        '/profile': (context) => Profile(),
+        '/info': (context) => Info(),
+        '/home': (context) => Homepage(),
         '/about': (context) => About(),
       },
       
