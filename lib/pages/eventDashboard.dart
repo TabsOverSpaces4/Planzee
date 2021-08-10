c40727cf981d0ac2b41107b317f4d49624a10efc
devanshi <devanshi.karthikk@gmail.com>
2021-08-10 23:00:02 +0530

    dashboard changes

--- lib/pages/eventDashboard.dart
+++ lib/pages/eventDashboard.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:planzee/widgets/bottomtabs.dart';
 import 'package:planzee/tabs/hometab.dart';
@@ -17,46 +18,191 @@ class _HomepageState extends State<Homepage> {
 
   @override
   Widget build(BuildContext context) {
-    final deviceWidth = MediaQuery.of(context).size.height;
-    final deviceHorizontal = MediaQuery.of(context).size.width;
+    final deviceHeight = MediaQuery.of(context).size.height;
+    final deviceWidth = MediaQuery.of(context).size.width;
     return Scaffold(
-      backgroundColor: Theme.of(context).accentColor,
-        body:Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Container(
-                child: Expanded(
-                  child: PageView(
-                    controller: _tabsPageController,
-                    onPageChanged: (num) {
-                      setState(() {
-                        _selectedTab = num;
-                      });
-                    },
-                    children: [
-                      HomeTab(),
-                      // Searchtab(),
-                      // Savedtab(),
-                      // Profiletab(),
-                    ],
-                  ),
-                )),
+      backgroundColor: Theme.of(context).primaryColor,
+      body:Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Container(
+            height: deviceHeight * .75,
+            decoration: BoxDecoration(
+              color: Theme.of(context).accentColor,
+              borderRadius: BorderRadius.only(
+                topRight: Radius.circular(50),
+                topLeft: Radius.circular(50)
 
-            Btmtabs(
-              selectedTab: _selectedTab,
-              tabPressed: (num) {
-                setState(() {
-                  _tabsPageController.animateToPage(num,
-                      duration: Duration(milliseconds: 300),
-                      curve: Curves.easeOutCubic);
-                });
-              },
+              ),
             ),
 
-          ],
-        )
+            child: ListView(
+              children: [
+                Container(
+                  padding: EdgeInsets.symmetric(vertical: 4, horizontal: 16),
+                  height: deviceHeight * .15,
+                  child: Card(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12)
+                    ),
+                    child: Center(
+                      child: Text(
+                        "Event Details",
+                        style: TextStyle(
+                          fontSize: 25,
+                          color: Theme.of(context).primaryColor
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.symmetric(vertical: 4, horizontal: 16),
+                  height: deviceHeight * .15,
+                  child: Card(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12)
+                    ),
+                    child: Center(
+                      child: Text(
+                        "Task Assignment",
+                        style: TextStyle(
+                            fontSize: 25,
+                            color: Theme.of(context).primaryColor
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.symmetric(vertical: 4, horizontal: 16),
+                  height: deviceHeight * .15,
+                  child: Card(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12)
+                    ),
+                    child: Center(
+                      child: Text(
+                        "Attendance",
+                        style: TextStyle(
+                            fontSize: 25,
+                            color: Theme.of(context).primaryColor
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.symmetric(vertical: 4, horizontal: 16),
+                  height: deviceHeight * .15,
+                  child: Card(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12)
+                    ),
+                    child: Center(
+                      child: Text(
+                        "Certificate Generator",
+                        style: TextStyle(
+                            fontSize: 25,
+                            color: Theme.of(context).primaryColor
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.symmetric(vertical: 4, horizontal: 16),
+                  height: deviceHeight * .15,
+                  child: Card(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12)
+                    ),
+                    child: Center(
+                      child: Text(
+                        "Budget Tracker",
+                        style: TextStyle(
+                            fontSize: 25,
+                            color: Theme.of(context).primaryColor
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.symmetric(vertical: 4, horizontal: 16),
+                  height: deviceHeight * .15,
+                  child: Card(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12)
+                    ),
+                    child: Center(
+                      child: Text(
+                        "Itenerary",
+                        style: TextStyle(
+                            fontSize: 25,
+                            color: Theme.of(context).primaryColor
+                        ),
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
 
+        ],
+      ),
     );
+
+
+
+
+
+
+
+
+
+
+
+
+    // return Scaffold(
+    //   backgroundColor: Theme.of(context).accentColor,
+    //     body:Column(
+    //       mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    //       children: [
+    //         Container(
+    //             child: Expanded(
+    //               child: PageView(
+    //                 controller: _tabsPageController,
+    //                 onPageChanged: (num) {
+    //                   setState(() {
+    //                     _selectedTab = num;
+    //                   });
+    //                 },
+    //                 children: [
+    //                   HomeTab(),
+    //                   // Searchtab(),
+    //                   // Savedtab(),
+    //                   // Profiletab(),
+    //                 ],
+    //               ),
+    //             )),
+    //
+    //         // Btmtabs(
+    //         //   selectedTab: _selectedTab,
+    //         //   tabPressed: (num) {
+    //         //     setState(() {
+    //         //       _tabsPageController.animateToPage(num,
+    //         //           duration: Duration(milliseconds: 300),
+    //         //           curve: Curves.easeOutCubic);
+    //         //     });
+    //         //   },
+    //         // ),
+    //
+    //       ],
+    //     )
+    //
+    // );
   }
 }
 
